84d7c55d4dff8e3587946e4dbb2e230a695b407f
dsjt <toot.daiylfalaiydt@gmail.com>
2017-06-07 11:06:12 +0900

    [add] configulation files

--- extract_result.py
+++ extract_result.py
@@ -32,7 +32,8 @@ def load_num_image(fn):
 def recognize_image(img):
     import sys
     templates,images = load_templates()
-    value = np.array(img.resize(regularized_number_image_size))
+    gray=ImageOps.grayscale(img)
+    value = np.array(gray.resize(regularized_number_image_size))
     bi_value = np.asarray(value < np.mean(value),dtype=int)
     score = [np.sum(np.logical_not(np.logical_xor(t,bi_value))) for t in templates]
     answer = np.argmax(score)
@@ -49,122 +50,41 @@ def load_configuration(fn=".crop_box.json"):
     return config
 
 def main(fn):
+    # 設定読み込み
+    config = load_configuration()
+
     # リザルト画像
     result = Image.open(fn)
-    grey = ImageOps.grayscale(result)
 
+    # データ初期化
     data = {}
+    for item in config:
+        if isinstance(config[item],list) and isinstance(config[item][0],list):
+            images=[]
+            for i,loc in enumerate(config[item]):
+                im = result.crop(loc)
+                im.save("tmp/{}{}.jpg".format(item,i))
+                images+=[im]
+            data[item]=int(recognize_item(images))
+        elif isinstance(config[item],list):
+            im = result.crop(config[item])
+            im.save("tmp/{}.jpg".format(item))
+            data[item]=None
+        elif isinstance(config[item],dict):
+            # dictの中に単一四角形は許していない
+            inner_data={}
+            for jtem in config[item]:
+                images=[]
+                for i,loc in enumerate(config[item][jtem]):
+                    im = result.crop(loc)
+                    im.save("tmp/{}{}.jpg".format(item,i))
+                    images+=[im]
+                inner_data[jtem]=int(recognize_item(images))
+            data[item]=inner_data
+        else:
+            print("?",item)
+            pass
 
-    # 曲名
-    title=result.crop((172,125,371,180))
-    data['title']=None
-    title.save("tmp/title.jpg")
-    # 難易度
-    difficulty=result.crop((173,100,269,120))
-    data['difficulty']=None
-    difficulty.save("tmp/difficulty.jpg")
-    # 楽曲Lv
-    lv=[grey.crop((419,103,430,118)),
-        grey.crop((431,103,442,118))]
-    data['level']=int(recognize_item(lv))
-    for i,l in enumerate(lv):
-        l.save("tmp/lv{}.jpg".format(i))
-
-    data['judge']={}
-    # perfect
-    perfect=[grey.crop((371,206,387,229)),
-             grey.crop((390,206,406,229)),
-             grey.crop((409,206,425,229)),
-             grey.crop((428,206,444,229))]
-    data['judge']['perfect']=int(recognize_item(perfect))
-    for i,l in enumerate(perfect):
-        l.save("tmp/perfect{}.jpg".format(i))
-
-    # great
-    great=[grey.crop((371,241,387,264)),
-           grey.crop((390,241,406,264)),
-           grey.crop((409,241,425,264)),
-           grey.crop((428,241,444,264))]
-    data['judge']['great']=int(recognize_item(great))
-    for i,l in enumerate(great):
-        l.save("tmp/great{}.jpg".format(i))
-    # nice
-    nice=[grey.crop((371,276,387,299)),
-          grey.crop((390,276,406,299)),
-          grey.crop((409,276,425,299)),
-          grey.crop((428,276,444,299))]
-    data['judge']['nice']=int(recognize_item(nice))
-    for i,l in enumerate(nice):
-        l.save("tmp/nice{}.jpg".format(i))
-
-    # bad
-    bad=[grey.crop((371,311,387,334)),
-         grey.crop((390,311,406,334)),
-         grey.crop((409,311,425,334)),
-         grey.crop((428,311,444,334))]
-    data['judge']['bad']=int(recognize_item(bad))
-    for i,l in enumerate(bad):
-        l.save("tmp/bad{}.jpg".format(i))
-    # miss
-    miss=[grey.crop((371,346,387,369)),
-          grey.crop((390,346,406,369)),
-          grey.crop((409,346,425,369)),
-          grey.crop((428,346,444,369))]
-    data['judge']['miss']=int(recognize_item(miss))
-    for i,l in enumerate(miss):
-        l.save("tmp/miss{}.jpg".format(i))
-
-    # combo
-    combo=[grey.crop((364,400,382,425)),
-           grey.crop((385,400,403,425)),
-           grey.crop((406,400,424,425)),
-           grey.crop((427,400,445,425))]
-    data['combo']=int(recognize_item(combo))
-    for i,l in enumerate(combo):
-        l.save("tmp/combo{}.jpg".format(i))
-
-    # fullcombo
-    full_combo=result.crop((372,368,477,396))
-    data['full_combo']=None
-    full_combo.save("tmp/full_combo.jpg")
-    # new record
-    new_record=result.crop((372,450,477,479))
-    data['new_record']=None
-    new_record.save("tmp/new_record.jpg")
-    # score
-    score=[grey.crop((301,480,319,506)),
-           grey.crop((322,480,340,506)),
-           grey.crop((343,480,361,506)),
-           grey.crop((364,480,382,506)),
-           grey.crop((385,480,403,506)),
-           grey.crop((406,480,424,506)),
-           grey.crop((427,480,445,506))]
-    data['score']=int(recognize_item(score))
-    for i,l in enumerate(score):
-        l.save("tmp/score{}.jpg".format(i))
-
-    # high_score
-    high_score=[grey.crop((321,521,337,542)),
-                grey.crop((339,521,355,542)),
-                grey.crop((357,521,373,542)),
-                grey.crop((375,521,391,542)),
-                grey.crop((393,521,409,542)),
-                grey.crop((411,521,427,542)),
-                grey.crop((429,521,445,542))]
-    data['high_score']=int(recognize_item(high_score))
-    for i,l in enumerate(high_score):
-        l.save("tmp/high_score{}.jpg".format(i))
-
-    # PRP
-    PRP=[grey.crop((332,573,347,592)),
-         grey.crop((348,573,363,592)),
-         grey.crop((364,573,379,592)),
-         grey.crop((380,573,395,592))]
-    data['PRP']=int(recognize_item(PRP))
-    for i,l in enumerate(PRP):
-        l.save("tmp/PRP{}.jpg".format(i))
-
-    import json
     print(json.dumps(data,indent=4))
     return data
 
